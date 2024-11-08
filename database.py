5a8391e43928f905beb92e483b0132b31ed37cb3
caiolaurinogomes <caio.laurino@push3.com.br>
2024-11-08 17:13:34 -0300

    Update database.py

--- database.py
+++ database.py
@@ -2,13 +2,19 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from getpass import getpass  # Importa a função getpass para solicitar a senha
 
-# Solicita a senha do usuário de forma segura
-password = getpass("Digite sua senha para conectar ao banco de dados: ")
+# Senha para conectar ao banco de dados
+# Para autenticação do Azure AD, a senha não é usada diretamente aqui
+# Utilize um token de autenticação se necessário
 
 # Configuração do banco de dados Azure SQL
-DATABASE_URL = f"mssql+pyodbc://caio.laurino:{password}@softwareprecificacao.database.windows.net/precificacao_db?driver=ODBC+Driver+17+for+SQL+Server"
+DATABASE_URL = (
+    "mssql+pyodbc://caio.laurino@push3.com.br@softwareprecificacao.database.windows.net/precificacao_db?"
+    "driver=ODBC+Driver+17+for+SQL+Server;"
+    "Authentication=ActiveDirectoryPassword;"
+    "UID=caio.laurino@push3.com.br;"
+    "PWD=Spfc1717!"
+)
 
 try:
     # Cria o engine do SQLAlchemy
@@ -24,6 +30,11 @@ Base = declarative_base()
 # Função para obter uma sessão de banco de dados
 def get_db():
     db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
     try:
         yield db
     finally:

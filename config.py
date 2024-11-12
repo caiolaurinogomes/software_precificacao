164a10e0846c2e7448e7e056f2c22c784b678d7d
caiolaurinogomes <caio.laurino@push3.com.br>
2024-11-12 14:44:59 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
 import os
 
 # Configurações do banco de dados Azure SQL
-DATABASE_USERNAME = "caio_laurino"  # Novo login para autenticação SQL
-DATABASE_PASSWORD = "Spfc1717!"  # Sua senha SQL
+DATABASE_USERNAME = "caio_laurino"  # Nome do usuário SQL
+DATABASE_PASSWORD = "Spfc1717!"  # Senha do usuário SQL
 DATABASE_SERVER = "softwareprecificacao.database.windows.net"  # Nome do servidor
 DATABASE_NAME = "db_precificacao"  # Nome do banco de dados
 
 # String de conexão para autenticação SQL
-SQLALCHEMY_DATABASE_URL = f"mssql+pyodbc://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_SERVER}/{DATABASE_NAME}?driver=ODBC+Driver+18+for+SQL+Server&Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
+SQLALCHEMY_DATABASE_URL = (
+    f"mssql+pyodbc://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_SERVER}:1433/{DATABASE_NAME}?driver=ODBC+Driver+18+for+SQL+Server&Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
+)

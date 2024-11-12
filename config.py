4938aa1fcdca941a868ab4337f4eac0d8979b66f
caiolaurinogomes <caio.laurino@push3.com.br>
2024-11-12 14:34:39 -0300

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 DATABASE_USERNAME = "caio_laurino"  # Novo login para autenticação SQL
 DATABASE_PASSWORD = "Spfc1717!"  # Sua senha SQL
 DATABASE_SERVER = "softwareprecificacao.database.windows.net"  # Nome do servidor
-DATABASE_NAME = "precificacao_db"  # Nome do banco de dados
+DATABASE_NAME = "db_precificacao"  # Nome do banco de dados
 
 # String de conexão para autenticação SQL
 SQLALCHEMY_DATABASE_URL = f"mssql+pyodbc://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_SERVER}/{DATABASE_NAME}?driver=ODBC+Driver+18+for+SQL+Server&Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
